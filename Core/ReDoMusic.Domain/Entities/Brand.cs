d71ee0d883d61f2430b04d6863edd23e7a1dbc11
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-10-23 19:59:10 +0300

    ReDoMusic

---
+++ Core/ReDoMusic.Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReDoMusic.Domain.Common;
+
+namespace ReDoMusic.Domain.Entities
+{
+    public class Brand : EntityBase<Guid>
+    {
+        public string Name { get; set; }
+        public string DisplayText { get; set; }
+        public string Address { get; set; }
+    }
+
+}
---
+++ Infrastructure/ReDoMusic.Persistence/Configurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ReDoMusic.Persistence
+{
+    public static class Configurations
+    {
+        public static string GetString(string key)
+        {
+            ConfigurationManager configurationManager = new ConfigurationManager();
+
+            string path = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName}\\Infrastructure\\ReDoMusic.Persistence";
+
+            configurationManager.SetBasePath(path);
+
+            configurationManager.AddJsonFile("PrivateInformations.json");
+
+            return configurationManager.GetSection(key).Value;
+
+        }
+    }
+}
